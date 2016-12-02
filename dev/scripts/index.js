e7a90b3151bf3e971b6fc44e86a3a13cb702a6f3
Trần Nguyên Trọng <trongtran19021997@gmail.com>
2016-12-03 06:47:07 +0700

    fixed CSS, add remodal

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -1,6 +1,47 @@
 require("./active.js");
 require("owl.carousel/dist/owl.carousel.js");
+require("remodal/dist/remodal.min.js");
 
+console.log('set up');
+$(document).ready(function() {
+    $(document).on('opening', '.remodal', function () {
+        console.log('Modal is opening');
+    });
+
+    $(document).on('opened', '.remodal', function () {
+        console.log('Modal is opened');
+    });
+
+    $(document).on('closing', '.remodal', function (e) {
+
+        // Reason: 'confirmation', 'cancellation'
+        console.log('Modal is closing' + (e.reason ? ', reason: ' + e.reason : ''));
+    });
+
+    $(document).on('closed', '.remodal', function (e) {
+
+        // Reason: 'confirmation', 'cancellation'
+        console.log('Modal is closed' + (e.reason ? ', reason: ' + e.reason : ''));
+    });
+
+    $(document).on('confirmation', '.remodal', function () {
+        console.log('Confirmation button is clicked');
+    });
+
+    $(document).on('cancellation', '.remodal', function () {
+        console.log('Cancel button is clicked');
+    });
+});
+
+
+$(document).ready(function() {
+    $(document).on('closed', '.remodal', function (e) {
+        $('#stop').on('click', function () {
+            //$('#popup-youtube-player').stopVideo();
+            $('#youtube-player')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+        })
+        });
+});
 $(document).ready(function(){
     $('.slider').owlCarousel({
         items: 1,
@@ -23,4 +64,6 @@ $(document).ready(function () {
         loop:true,
         autoplay: true,
     })
-})
+});
+
+
